b6a71f8acdbc009f79bd425f7de9e160a404cb4f
unknown <Markbalangue84@gmail.com>
2023-12-12 21:40:16 +0800

    Second commit

---
+++ Constants.php
@@ -0,0 +1,10 @@
+<?php
+define("MINSIZE", 50);
+echo MINSIZE;
+echo constant("MINSIZE"); 
+
+define("ONE", "first thing");
+define("TWO2", "second thing");
+define("THREE_3", "third thing");
+define("_2TWO", "second thing");
+define("_THREE_", "third value");
